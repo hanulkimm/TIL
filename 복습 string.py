7d1dbad4e859e796be16c25b0cd7298f8426e231
hanulkim <hanulkim2000@naver.com>
2023-02-12 23:34:29 +0900

    update 복습str

--- 복습 string.py
+++ 복습 string.py
@@ -1,3 +1,39 @@
+## 어디에 단어가 들어갈 수 있을까 ##
+def count(arr):
+    ans = 0
+    for lst in arr:
+        cnt = 0
+        for i in lst:
+            if i == 1:
+                cnt += 1
+            else:
+                if cnt == k:
+                    ans += 1
+                cnt = 0
+    return ans
+t = int(input())
+for tc in range(1, t+1):
+    n, k = map(int, input().split())
+    arr = [list(map(int, input().split())) + [0] for _ in range(n) ] + [[0] * n]
+    arr_t = list(zip(*arr))
+    ans = count(arr) + count(arr_t)
+
+
+    print(f'#{tc} {ans}')
+
+
+## 간단한 압축 풀기 ##
+t = int(input())
+for tc in range(1, t+1):
+    n = int(input())
+    st = ''
+    for _ in range(n):
+        ch, cnt = input().split()
+        st += ch * int(cnt)
+    print(f'#{tc}')
+    for i in range(0,len(st),10):
+        print(st[i:i+10])
+        
 ## 회문 ##
 t = int(input())
 for tc in range(1,t+1):
@@ -25,6 +61,28 @@ for tc in range(1, 11):
                     ans += 1
         arr = list(zip(*arr))
     print(f'#{tc} {ans}')
+
+
+## 회문 2 ##
+def isPar(arr, m):
+    for lst in arr:
+        for i in range(n-m+1):
+            if lst[i:i+m] == lst[i:i+m][::-1]:
+                return True
+    return False
+
+
+for tc in range(1, 11):
+    _ = int(input())
+    n = 100
+    arr = [list(input()) for _ in range(n)]
+    arr_t = list(zip(*arr))
+
+    for m in range(n, 0, -1):
+        if isPar(arr, m) or isPar(arr_t,m):
+            break
+    print(f'#{tc} {m}')
+
 
 ## 문자열의 거울상 ##
 t = int(input())
