f92c7df75e7901d3058a15f922bce70a147f29b3
hanulkimm <hanulkim2000@naver.com>
2023-07-09 19:54:30 +0900

    update

---
+++ React/mini-blog/src/component/list/PostListItem.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components"
+
+function PostlistItem(props) {
+    const {post, onClick} = props;
+
+    return(
+        <Wrapper onClick={onClick}>
+            
+        </Wrapper>
+    )
+}
